0e4edd2b09bdcc830882fc7c6158e03de2504de6
NickSeyler <nickseyler25@gmail.com>
2022-02-10 19:45:59 -0500

    Added ability to delete books

--- js/site.js
+++ js/site.js
@@ -1,65 +1,74 @@
 const books = [{
+        id: 1,
         title: "The Hobbit",
         author: "J.R.R. Tolkien",
         copiesSold: 104000000,
         country: "United Kingdom",
-        publishingDate: "09/21/1937",
+        publishingDate: "09/21/1937"
     },
     {
+        id: 2,
         title: "The Fellowship of the Ring",
         author: "J.R.R. Tolkien",
         copiesSold: 161000000,
         country: "United Kingdom",
-        publishingDate: "07/29/1954",
+        publishingDate: "07/29/1954"
     },
     {
+        id: 3,
         title: "The Silmarillion",
         author: "J.R.R. Tolkien",
         copiesSold: 1200000,
         country: "United Kingdom",
-        publishingDate: "09/15/1977",
+        publishingDate: "09/15/1977"
     },
     {
+        id: 4,
         title: "The Cat in the Hat",
         author: "Dr. Seuss",
         copiesSold: 16500000,
         country: "United States of America",
-        publishingDate: "03/12/1957",
+        publishingDate: "03/12/1957"
     },
     {
+        id: 5,
         title: "Green Eggs and Ham",
         author: "Dr. Seuss",
         copiesSold: 18000000,
         country: "United States of America",
-        publishingDate: "08/12/1960",
+        publishingDate: "08/12/1960"
     },
     {
+        id: 6,
         title: "One Fish, Two Fish, Red Fish, Blue Fish",
         author: "Dr. Seuss",
         copiesSold: 13700000,
         country: "United States of America",
-        publishingDate: "05/14/1960",
+        publishingDate: "05/14/1960"
     },
     {
+        id: 7,
         title: "David Copperfield",
         author: "Charles Dickens",
         copiesSold: 85000000,
         country: "United Kingdom",
-        publishingDate: "11/07/1849",
+        publishingDate: "11/07/1849"
     },
     {
+        id: 8,
         title: "A Tale of Two Cities",
         author: "Charles Dickens",
         copiesSold: 223000000,
         country: "United Kingdom",
-        publishingDate: "11/25/1859",
+        publishingDate: "11/25/1859"
     },
     {
+        id: 9,
         title: "Great Expectations",
         author: "Charles Dickens",
         copiesSold: 165000000,
         country: "United Kingdom",
-        publishingDate: "06/01/1861",
+        publishingDate: "06/01/1861"
     }
 ];
 
@@ -171,6 +180,8 @@ function displayData(){
     let bookDataBody = document.getElementById("bookDataBody");
     bookDataBody.innerHTML="";
 
+    let delButton = `<button type="button" class="btn btn-danger" onclick="deleteData(this)">Delete</button>`;
+
     let currentBooks = JSON.parse(localStorage.getItem("booksArray")) || [];
 
     if (currentBooks.length == 0){
@@ -181,13 +192,16 @@ function displayData(){
     for (let i = 0; i < currentBooks.length; i++){
         let bookRow = document.importNode(template.content, true);
         let bookCols = bookRow.querySelectorAll("td");
+        let bookTr = bookRow.querySelector("tr");
 
         bookCols[0].textContent = currentBooks[i].title;
         bookCols[1].textContent = currentBooks[i].author;
         bookCols[2].textContent = currentBooks[i].copiesSold;
         bookCols[3].textContent = currentBooks[i].country;
         bookCols[4].textContent = new Date(currentBooks[i].publishingDate).toLocaleDateString();
+        bookCols[5].innerHTML = delButton;
 
+        bookTr.setAttribute("data-id", currentBooks[i].id);
         bookDataBody.appendChild(bookRow);
     }
 }
@@ -199,6 +213,22 @@ function saveData(){
     let currentBooks = JSON.parse(localStorage.getItem("booksArray")) || books;
     let obj = {};
 
+    let bookid = 1;
+    //generate next id for book
+    if (currentBooks == 0) {
+        bookid = 1;
+    } else {
+        let bookIDArray = currentBooks.map(
+            function (obj) {
+                return obj.id;
+            });
+
+        //find the maximum in the array
+        let maxid = Math.max.apply(null, bookIDArray);
+        bookid = maxid + 1;
+    }
+
+    obj["id"] = bookid;
     obj["title"] = document.getElementById("newTitle").value;
     obj["author"] = document.getElementById("newAuthor").value;
     obj["copiesSold"] = parseInt(document.getElementById("newCopiesSold").value, 10);
@@ -209,6 +239,28 @@ function saveData(){
     obj["publishingDate"] = new Date(publishingDate2).toLocaleDateString();
 
     currentBooks.push(obj);
+    localStorage.setItem("booksArray", JSON.stringify(currentBooks));
+
+    buildDropDown();
+    displayData();
+}
+
+//expected input: button
+//expected reutrn: none
+//delete data from the webpage and the database
+function deleteData(delBtn){
+    let currentBooks = JSON.parse(localStorage.getItem("booksArray")) || books;
+
+    let row = delBtn.parentElement.parentElement;
+    let bookid = row.getAttribute("data-id");
+
+    row.remove();
+
+    //find index of item in array
+    let bookIndex = currentBooks.findIndex(obj => obj.id == bookid);
+    currentBooks.splice(bookIndex, 1);
+
+    //push the new values back to local storage
     localStorage.setItem("booksArray", JSON.stringify(currentBooks));
 
     buildDropDown();
